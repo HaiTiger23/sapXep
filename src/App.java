6c00383b8e1be0485eca71dfb0d3e2c3203b6ae4
Hai Tran <hai@gmail.com>
2023-04-23 15:08:40 +0700

    commit 2

--- src/App.java
+++ src/App.java
@@ -1,10 +1,15 @@
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
         int[] a = inputData();
-        sapXepChen(a);
+        // mergeSort(a, 0, a.length);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 
     // SapXepChon
@@ -32,12 +37,12 @@ public class App {
         for (int i = 1; i < a.length; i++) {
             int j = i - 1;
             int key = a[i];
-           
+
             while (j >= 0 && a[j] > key) {
                 a[j + 1] = a[j];
                 j = j - 1;
             }
-           
+
             a[j + 1] = key;
         }
         for (int i = 0; i < a.length; i++) {
@@ -58,6 +63,29 @@ public class App {
             System.out.println(a[i]);
         }
     }
+    // QuickSort
+    public static int partition(int[] a, int i , int j) {
+        int pivot = a[j];
+        int low = i;
+        int high = j -1;
+        while(true) {
+            while(low <= high && a[low] < pivot) low++;
+            while (high >= low && a[high] > pivot) high--;
+            if(low >= high) break;
+            swap(a, low, high);
+            low++;
+            high--;
+        }
+        swap(a, low, j); 
+        return low;
+    }
+    public static void quickSort(int[] a, int i, int j) {
+        if (i < j) {
+           int pi = partition(a, i, j);
+           quickSort(a, i, pi -1);
+           quickSort(a, pi + 1, j);
+        }
+    }
 
     public static void swap(int a[], int i, int k) {
         int temp = a[i];
@@ -65,12 +93,52 @@ public class App {
         a[k] = temp;
     }
 
+    // //Merge sort
+    // public static void mergeSort(int[] a, int l, int r) {
+    //     if(l >= r) return;
+    //     int mid = (l + r) / 2;
+    //     mergeSort(a,l,mid);
+    //     mergeSort(a,mid+1,r);
+    //     merge(a,l,mid,r);
+    // }
+    // public static void merge(int[] a, int l, int m, int r) {
+    //     int[] x = Arrays.copyOfRange(a,l, m);
+    //     int[] y = Arrays.copyOfRange(a,m,r);
+    //     System.out.println(Arrays.toString(x)+":"+Arrays.toString(y));
+    //     int i = 0, j = 0;
+    //     while (i < x.length && j <y.length) {
+    //         if(a[i] <= y[j]) {
+    //             a[l] = x[i];
+    //             l++;
+    //             i++;
+    //         }else {
+    //             a[l] = y[j];
+    //             l++;
+    //             j++;
+    //         }
+    //     }
+    //     while(i < x.length) {
+    //         a[l] = x[i];
+    //         l++;
+    //         i++;
+    //     }
+    //     while(j < y.length) {
+    //         a[l] = y[j];
+    //         l++;
+    //         j++;
+    //     }
+        
+
+        
+    // }
+
     public static int[] inputData() {
         String myfile = "F:/HocTap/UDThuatToan/ontap/myArray.txt";
         int[] array = new int[1];
         try (BufferedReader reader = new BufferedReader(new FileReader(myfile))) {
             String line;
             int rows = 0;
+            int j = 0;
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split(" ");
                 if (rows == 0) {
@@ -78,7 +146,8 @@ public class App {
                     rows = 1;
                 } else {
                     for (int i = 0; i < values.length; i++) {
-                        array[i] = Integer.parseInt(values[i]);
+                        array[j] = Integer.parseInt(values[i]);
+                        j++;
                     }
                 }
             }
